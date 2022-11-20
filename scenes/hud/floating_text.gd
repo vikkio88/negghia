626a5d233bb7d52de719ce064ff4c2bf439d291c
vikkio <vincenzo.ciaccio@gmail.com>
2022-11-20 10:48:28 +0000

    - added footstep sound - fixed bug on floating text disappearing

--- scenes/hud/floating_text.gd
+++ scenes/hud/floating_text.gd
@@ -4,6 +4,8 @@ extends Node2D
 
 
 func trigger(text: String, duration: float = .7, destroy: bool = true):
+	# maybe there is a better way of doing this
+	z_index = 100
 	label.text = text
 	var tween = create_tween()
 	tween.tween_property(self, "position:y", position.y - 40, duration)
